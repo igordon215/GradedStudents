77a282e3db02f8b3a76f04499f9ba5a2f289a3d3
igordon215 <igordon215@gmail.com>
2024-08-02 13:12:55 -0400

    classRoomTest1

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -19,5 +19,32 @@ public class Classroom {
         students = new Student[30];
     }
 
+    public String getStudents() {
+        String studentList = "Students: \n";
+        for(int i  = 0; i < students.length; i++) {
+            if(students[i] != null) {
+                studentList += String.format("\t%s %s\n", students[i].getFirstName(), students[i].getLastName());
+            }
+        }
+        return studentList;
+    }
+
+    public int numberOfStudents() {
+        return students.length;
+    }
+
+    public double getAverageExamScore() {
+        double totalAvgScore = 0;
+        double avgAvgScore = 0;
+
+        for(int i = 0; i < students.length; i++) {
+            totalAvgScore += students[i].getAverageExamScore();
+        }
+
+        avgAvgScore = totalAvgScore/students.length;
+
+        return  avgAvgScore;
+    }
+
 
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,81 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ClassroomTest {
+
+    @Test
+    public void testClassroomConstructor() {
+        // : Given
+        Classroom nullClass = new Classroom();
+
+        // : When
+        int expected = 30;
+        int actual = nullClass.numberOfStudents();
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testClassroomConstructorWithMaxStudents() {
+        // : Given
+        Classroom maxClass = new Classroom(69);
+
+        // : When
+        int expected = 69;
+        int actual = maxClass.numberOfStudents();
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testClassroomConstructorWithStudentsArray() {
+        // : Given
+        Double[] examScores = {100.0, 100.0, 90.0, 100.0, 93.0};
+        Double[] examScores2 = {75.0, 58.0, 84.0, 96.0, 98.0};
+        Double[] examScores3 = {55.0, 65.0, 75.0, 85.0, 95.0};
+        Student student1 = new Student("student","one", examScores);
+        Student student2 = new Student("student","two", examScores2);
+        Student student3 = new Student("student","three", examScores3);
+        Student[] zipCode = {student1, student2, student3};
+
+        Classroom codeClass = new Classroom(zipCode);
+
+        // : When
+        int expected = 3;
+        int actual = codeClass.numberOfStudents();
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetStudents() {
+        // : Given
+        Double[] examScores = {100.0, 100.0, 90.0, 100.0, 93.0};
+        Double[] examScores2 = {75.0, 58.0, 84.0, 96.0, 98.0};
+        Double[] examScores3 = {55.0, 65.0, 75.0, 85.0, 95.0};
+        Student student1 = new Student("student","one", examScores);
+        Student student2 = new Student("student","two", examScores2);
+        Student student3 = new Student("student","three", examScores3);
+        Student[] zipCode = {student1, student2, student3};
+
+        Classroom codeClass = new Classroom(zipCode);
+
+        // : When
+        String expected = String.format("Students: \n" +
+                "\tstudent one\n" +
+                "\tstudent two\n" +
+                "\tstudent three\n");
+        String actual = codeClass.getStudents();
+        System.out.println(expected);
+        System.out.println(actual);
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
