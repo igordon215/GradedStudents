71dfbad3ead0cdd0b8ea76e9e63af5768240bf40
igordon215 <igordon215@gmail.com>
2024-08-02 11:15:41 -0400

    studentNameTests

--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,75 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
+
+    @Test
+    public void testGetFirstName() {
+        // : Given
+        String firstName = "Bill";
+        String lastName = "Nye";
+        Double[] examScores = {100.0, 100.0, 90.0, 100.0, 93.0};
+        Student bill = new Student(firstName, lastName, examScores);
+
+        // : When
+        String expected = "Bill";
+        String actual = bill.getFirstName();
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetLastName() {
+        // : Given
+        String firstName = "Bill";
+        String lastName = "Nye";
+        Double[] examScores = {100.0, 100.0, 90.0, 100.0, 93.0};
+        Student bill = new Student(firstName, lastName, examScores);
+
+        // : When
+        String expected = "Nye";
+        String actual = bill.getLastName();
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetFirstName() {
+        // : Given
+        String firstName = "Bill";
+        String lastName = "Nye";
+        Double[] examScores = {100.0, 100.0, 90.0, 100.0, 93.0};
+        Student bill = new Student(firstName, lastName, examScores);
+
+        // : When
+        bill.setFirstName("Bill");
+        String expected = "Bill";
+        String actual = bill.getFirstName();
+
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetLastName() {
+        // : Given
+        String firstName = "Bill";
+        String lastName = "Nye";
+        Double[] examScores = {100.0, 100.0, 90.0, 100.0, 93.0};
+        Student bill = new Student(firstName, lastName, examScores);
+
+        // : When
+        bill.setLastName("Nye");
+        String expected = "Nye";
+        String actual = bill.getLastName();
+
+        // : Then
+        Assert.assertEquals(expected, actual);
+    }
 
 }
