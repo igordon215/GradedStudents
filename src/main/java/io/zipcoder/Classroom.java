3f3ce4455ae3bb7dd1be3b073ba95643326ade9e
igordon215 <igordon215@gmail.com>
2024-08-02 12:00:53 -0400

    startClassroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Classroom {
+    public Student[] students;
+    public HashMap<Character, ArrayList<Student>> gradedStudents = new HashMap<Character, ArrayList<Student>>();
+
+    public Classroom(int maxNumberOfStudents) {
+        students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+
+    public Classroom() {
+        students = new Student[30];
+    }
+
+
 }
